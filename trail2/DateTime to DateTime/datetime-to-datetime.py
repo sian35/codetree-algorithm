15e19b679ab59b53dc71ac593ac16b907498aed8
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-21 22:39:55 +0900

    [Passed] DateTime to DateTime (53ms, 16MB)

---
+++ trail2/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+day = a-11
+
+st_time = 11*60+11 + 11*24*60
+end_time = b*60 + c + 24*a*60
+
+if (end_time - st_time) <0:
+    print(-1)
+else:
+    print(end_time-st_time)
+
