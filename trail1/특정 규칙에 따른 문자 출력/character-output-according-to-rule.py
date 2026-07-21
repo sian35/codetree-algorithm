9e85d2f905d49e0cd354d1cb6b6bfeb123cdb73c
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-22 08:54:05 +0900

    [Passed] 특정 규칙에 따른 문자 출력 (53ms, 16MB)

---
+++ trail1/특정 규칙에 따른 문자 출력/character-output-according-to-rule.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+for i in range(N-1,-1,-1):
+    for _ in range(i):
+        print(' ', end=' ')
+    for _ in range(N-i):
+        print('@', end=' ')
+    print()
+
+for i in range(1,N):
+    for _ in range(N-i):
+        print('@', end=' ')
+    for _ in range(i):
+        print(' ', end=' ')
+    print()
