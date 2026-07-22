864769eed7721465ce615320f75963e4657353c1
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-22 19:14:58 +0900

    [Passed] 여러가지 진수변환 (51ms, 16MB)

---
+++ trail2/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,13 @@
+N, B = map(int, input().split())
+
+# Please write your code here.
+digits=[]
+while True:
+    if N < B:
+        digits.append(N)
+        break
+    digits.append(N%B)
+    N = N//B
+
+for d in digits[::-1]:
+    print(d, end='')
