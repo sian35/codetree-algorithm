b7ac1f36d52c12131395b45d2130c8beae55a859
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-26 16:20:09 +0900

    [Passed] 연속부분수열일까 (50ms, 16MB)

--- trail1/연속부분수열일까/contiguous-array-or-not.py
+++ trail1/연속부분수열일까/contiguous-array-or-not.py
@@ -2,31 +2,6 @@ n1, n2 = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-#4 4 1 4
-#4 3
-
-# if b[0] in a:
-#     curr = a.index(b[0])
-#     cnt=0
-#     while curr < len(a) and (len(a)-curr)<=len(b):
-#         print(a[curr], b[0])
-#         if a[curr] == b[0]:
-#             cnt=0
-#             for i in range(len(b)):
-#                 if a[curr] != b[i]:
-#                     break
-#                 else:
-#                     cnt+=1
-#                     curr+=1
-#         else:
-#             curr+=1
-#     if cnt == len(b):
-#         print("Yes")
-#     else:
-#         print("No")
-# else:
-#     print("No")
-
 cnt=0
 for i in range(len(a)-len(b)+1):
     if a[i] == b[0]:
