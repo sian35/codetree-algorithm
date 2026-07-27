9c7cc4ddf2dfb87ea3f961146d07fe3650100f3d
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-27 18:38:27 +0900

    [Passed] 자동차 단일 거래 이익 최대화하기 (55ms, 16MB)

--- trail1/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ trail1/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -5,7 +5,7 @@ arr = list(map(int, input().split()))
 buy = arr[0]
 profit =0
 for i in range(len(arr)):
-    for j in range(i, len(arr)):
+    for j in range(i+1, len(arr)):
         if arr[j] > arr[i] and profit < (arr[j]-arr[i]):
             profit = arr[j]-arr[i]
 
