67ecfa71ab3200d95175460cf39bab5733ab9c02
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-29 10:10:15 +0900

    [Passed] 동전이 있는 위치 (48ms, 16MB)

---
+++ trail1/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,11 @@
+n,m = map(int, input().split())
+arr = [[0 for _ in range(n)] for _ in range(n)]
+
+for i in range(m):
+    r,c = map(int, input().split())
+    arr[r-1][c-1] = 1
+
+for row in arr:
+    for r in row:
+        print(r, end=' ')
+    print()
