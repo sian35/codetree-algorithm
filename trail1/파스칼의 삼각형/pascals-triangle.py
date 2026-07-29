b3d1adb67016f2852516c7fb222d9f3f7431453d
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-29 09:45:17 +0900

    [Passed] 파스칼의 삼각형 (55ms, 16MB)

---
+++ trail1/파스칼의 삼각형/pascals-triangle.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+for i in range(n):
+    arr[i][0] = 1
+    arr[i][i] = 1
+
+for i in range(1,n):
+    for j in range(1,i+1):
+        arr[i][j] = arr[i-1][j-1]+arr[i-1][j]
+
+
+for i,row in enumerate(arr):
+    for r in row[:i+1]:
+        print(r, end=' ')
+    print()
