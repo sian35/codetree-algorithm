e6441bb9c80f2e1ceefd2cf7906ab9f7252d1983
sian35 <69207672+sian35@users.noreply.github.com>
2026-07-29 10:14:40 +0900

    [Passed] 격자에 점 그리기 (50ms, 16MB)

---
+++ trail1/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,12 @@
+n,m = map(int, input().split())
+arr = [[0 for _ in range(n)] for _ in range(n)]
+cnt=1
+for _ in range(m):
+    r,c = map(int, input().split())
+    arr[r-1][c-1] = cnt
+    cnt+=1
+
+for row in arr:
+    for r in row:
+        print(r, end=' ')
+    print()
