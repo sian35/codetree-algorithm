7df0ec706907f52085c8f6b4d6496dbb52bd7f04
sian35 <69207672+sian35@users.noreply.github.com>
2026-08-14 13:15:43 +0900

    [Passed] 특정 규칙에 따른 숫자 출력 (48ms, 16MB)

---
+++ trail1/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt=1
+for i in range(n):
+    for j in range(n):
+        if j<i:
+            print(' ', end=' ')
+        else:
+            print(cnt, end=' ')
+            cnt+=1
+            if cnt >9:
+                cnt = 1
+    print()
